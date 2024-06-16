15f2886774f594a9ce359be0b7a62776753502d9
Anno Knierim <anno.knierim@tu-dortmund.de>
2024-06-16 21:53:04 +0200

    Remove old dataset download bash script

--- init_dataset.sh
+++
@@ -1,7 +0,0 @@
-!/bin/sh
-
-wget -P data https://cdsarc.cds.unistra.fr/ftp/I/196/main.dat.gz
-wget -P data https://cdsarc.cds.unistra.fr/ftp/I/196/notes.dat.gz
-
-gzip -d data/main.dat.gz
-gzip -d data/notes.dat.gz
