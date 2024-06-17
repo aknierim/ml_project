3a2353a3bfefe29dc5c1e17d4ec0978df5304041
Anno Knierim <anno.knierim@tu-dortmund.de>
2024-06-17 15:37:53 +0200

    Add get dataset function to training utils

--- rolf/training/utils.py
+++ rolf/training/utils.py
@@ -1,6 +1,8 @@
 import sys
+from pathlib import Path
 
 import click
+import pandas as pd
 
 
 def save_on_interrupt(model_path):
@@ -19,3 +21,23 @@ def save_on_interrupt(model_path):
         print(f"Saving model to file {model_path} after {'EPOCHS'} epoch(s).")
 
     sys.exit(1)
+
+
+def get_train_test_dataset(path: str | Path):
+    """Get train/test datasets.
+
+    Parameters
+    ----------
+    path : str or Path
+        Path to the full dataset.
+    """
+    if not isinstance(path, Path):
+        path = Path(path)
+
+    try:
+        data = pd.read_hdf(path)
+    except Exception as e:
+        print(f"{e}: Fallback to pd.read_csv!")
+        data = pd.read_csv(path)
+
+    return data
