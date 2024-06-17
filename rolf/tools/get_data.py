a214f98005e82ff1b22191ca12daa729f638cc55
Anno Knierim <anno.knierim@tu-dortmund.de>
2024-06-17 15:55:02 +0200

    Add data tool

---
+++ rolf/tools/get_data.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+
+import click
+
+from rolf.data import GetData
+
+
+@click.command()
+@click.option(
+    "--output-directory",
+    "-o",
+    type=click.Path(exists=False, dir_okay=True),
+    help="Output directory for the dataset.",
+)
+@click.option(
+    "--url",
+    "-u",
+    type=str,
+    help="URL leading to the dataset.",
+    default=None,
+)
+@click.option(
+    "--from-name",
+    "-n",
+    is_flag=True,
+    help="Downloads the Radio Galaxy Dataset from a list of URLs.",
+    default=False,
+)
+def main(output_directory, url, from_name):
+    if not isinstance(output_directory, Path):
+        output_directory = Path(output_directory)
+
+    if output_directory.is_file():
+        raise ValueError("Output directory is not a directory and already exists")
+
+    if not output_directory.is_dir():
+        output_directory.mkdir(parents=True)
+
+    data = GetData(output_directory)
+
+    if url:
+        data.from_url(url)
+
+    elif from_name:
+        data.from_name()
+
+    else:
+        raise ValueError(
+            "Please either provide a valid URL or use the --from-name flag"
+            " to download the Radio Galaxy Dataset."
+        )
+
+
+if __name__ == "__main__":
+    main()
