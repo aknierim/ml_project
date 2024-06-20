3cd1e3918936bb2a7cbddc6d93eca25e772b1937
Christian Franz Wolfgang Arauner <christian.arauner@tu-dortmund.de>
2024-06-20 09:58:12 +0200

    added draft for toml reader

---
+++ rolf/tools/toml_reader.py
@@ -0,0 +1,27 @@
+import tomli
+from pathlib import Path
+
+class ReadConfig:
+    def __init__(self, config_path) -> None:
+        self.config_path = Path(config_path)
+        with open(self.config_path, "rb") as f:
+            self.toml = tomli.load(f)
+        self.paths = self.toml["paths"]
+        self.mode = self.toml["mode"]
+        self.meta = self.toml["meta"]
+        self.eval = self.toml["eval"]
+
+    def training(self) -> dict:
+        self.training_config = {}
+        self.training_config["paths"] = {"data": Path(self.paths["data"]), 
+                                         "model": Path(self.paths["model"]),
+                                         }
+        self.training_config["mode"] = self.mode
+        self.training_config["parameters"] = self.meta
+
+        return self.training_config
+    
+    def validation(self) -> dict:
+        self.validation_config = self.eval
+
+        return self.validation_config
