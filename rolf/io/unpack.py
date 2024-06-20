07329c2b29200dd12b71c2b4daa609e419eb0d13
Anno Knierim <anno.knierim@tu-dortmund.de>
2024-06-20 12:54:05 +0200

    Make unpacker a cli tool

--- rolf/io/unpack.py
+++ rolf/io/unpack.py
@@ -1,8 +1,26 @@
 from pathlib import Path
-from zipfile import ZipFile 
+from zipfile import ZipFile
 
+import click
+
+
+@click.command()
+@click.argument(
+    "source_path",
+    type=click.Path(exists=True, dir_okay=False, file_okay=True),
+)
+@click.option(
+    "--dest_path",
+    "-o",
+    type=click.Path(dir_okay=True, file_okay=False),
+    help="Output path to unpack to.",
+)
 def unzip(source_path, dest_path) -> None:
     source_path = Path(source_path)
     dest_path = Path(dest_path)
-    with ZipFile(source_path, 'r') as file:
+    with ZipFile(source_path, "r") as file:
         file.extractall(dest_path)
+
+
+if __name__ == "__main__":
+    unzip()
