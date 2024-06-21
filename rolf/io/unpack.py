21023191a66bc1ef5dffc90e665c46d1f45db7af
Anno Knierim <anno.knierim@tu-dortmund.de>
2024-06-21 12:07:29 +0200

    Update unpacker

--- rolf/io/unpack.py
+++ rolf/io/unpack.py
@@ -4,17 +4,6 @@ from zipfile import ZipFile
 import click
 
 
-@click.command()
-@click.argument(
-    "source_path",
-    type=click.Path(exists=True, dir_okay=False, file_okay=True),
-)
-@click.option(
-    "--dest_path",
-    "-o",
-    type=click.Path(dir_okay=True, file_okay=False),
-    help="Output path to unpack to.",
-)
 def unzip(source_path, dest_path) -> None:
     source_path = Path(source_path)
     dest_path = Path(dest_path)
@@ -22,5 +11,20 @@ def unzip(source_path, dest_path) -> None:
         file.extractall(dest_path)
 
 
+@click.command()
+@click.argument(
+    "input",
+    type=click.Path(exists=True, dir_okay=False, file_okay=True),
+)
+@click.option(
+    "--output",
+    "-o",
+    type=click.Path(dir_okay=True, file_okay=False),
+    help="Output path to unpack to.",
+)
+def main(input, output):
+    unzip(input, output)
+
+
 if __name__ == "__main__":
-    unzip()
+    main()
