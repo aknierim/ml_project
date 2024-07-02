df334ee0419e3aa73f8b4f580cc81036895e2489
Christian Franz Wolfgang Arauner <christian.arauner@tu-dortmund.de>
2024-07-02 16:23:40 +0200

    added random forest model

---
+++ rolf/romf/model.py
@@ -0,0 +1,44 @@
+"""Random Forest model"""
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from pathlib import Path
+from joblib import load, dump
+import sklearn
+import numpy as np
+
+
+class RandomForest:
+    def __init__(self, model_path) -> None:
+        self.model_path = Path(model_path)
+
+    def make_model(self, parameters, **kwargs) -> None:
+        self.model = RandomForestClassifier(**parameters, **kwargs)
+        self.parameters = self.model.get_params()
+
+    def load_model(self) -> None:
+        with open(self.model_path, "rb") as f:
+            self.model = load(f)
+
+    def get_model(self) -> sklearn.ensemble._forest.RandomForestClassifier:
+        return self.model
+
+    def fit_model(self, X_train, y_train) -> None:
+        self.model.fit(X_train, y_train)
+
+    def fit(self, X_train, y_train) -> None:
+        self.model.fit(X_train, y_train)
+
+    def get_params(self, **kwargs):
+        return self.model.get_params(**kwargs)
+    
+    def predict_model(self, X_test) -> np.ndarray:
+        self.y_pred = self.model.predict(X_test)
+        return self.y_pred
+
+    def evaluate_model(self, y_test) -> float:
+        return accuracy_score(self.y_pred, y_test)
+
+    def save_model(self) -> None:
+        with open(self.model_path, "wb") as f:
+            dump(self.model, f)
