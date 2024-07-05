220feea80bf81451e6fb6ee502fce7aa26e3affe
Anno Knierim <anno.knierim@tu-dortmund.de>
2024-07-05 12:34:28 +0200

    Move rolfo to new optim submodule

--- rolf/optim/rolfo.py
+++ rolf/optim/rolfo.py
@@ -9,8 +9,13 @@ from rolf.training.training import train_model
 
 
 class ParameterOptimization:
-
-    def __init__(self, optim_conf_path, optuna_path, data_path, random_state=None) -> None:
+    def __init__(
+        self,
+        optim_conf_path: str | Path,
+        optuna_path: str | Path,
+        data_path: str | Path,
+        random_state: int | np.random.RandomState = None,
+    ) -> None:
         self.optim_conf_path = Path(optim_conf_path)
         reader = ReadConfig(self.optim_conf_path)
         self.model_config = reader.training()
@@ -26,31 +31,46 @@ class ParameterOptimization:
             self.random_state = random_state
         else:
             raise TypeError(
-                "random_state has to be None, int or numpy.random.mtrand.RandomState instance!"
+                "random_state has to be None, int or "
+                "numpy.random.mtrand.RandomState instance!"
             )
-    
-    def load_data(self, image_path) -> None:
-        self.data = ReadHDF5(self.data_path, validation_ratio=0.1, test_ratio=0.05, random_state=self.random_state)
-        self.train_loader, self.val_loader, self.test_loader = self.data.create_data_loaders(batch_size=self.model_config["batch_size"], img_dir=image_path)
 
-    def make_network(self, trial : Trial) -> None:
-        
+    def load_data(self, image_path: str | Path) -> None:
+        self.data = ReadHDF5(
+            self.data_path,
+            validation_ratio=0.1,
+            test_ratio=0.05,
+            random_state=self.random_state,
+        )
+        (
+            self.train_loader,
+            self.val_loader,
+            self.test_loader,
+        ) = self.data.create_data_loaders(
+            batch_size=self.model_config["batch_size"], img_dir=image_path
+        )
+
+    def make_network(self, trial: Trial) -> None:
         self.use_tuning = {}
         for key in self.tuning_config:
             pars = self.tuning_config[key]
             if isinstance(pars, tuple):
                 if np.all([isinstance(pars[i], int) for i in range(3)]):
-                    self.use_tuning[key] = trial.suggest_int(key, low=pars[0], high=pars[1], step=pars[2])
+                    self.use_tuning[key] = trial.suggest_int(
+                        key, low=pars[0], high=pars[1], step=pars[2]
+                    )
                 elif np.all([isinstance(pars[i], float) for i in range(3)]):
-                    self.use_tuning[key] = trial.suggest_float(key, low=pars[0], high=pars[1], step=pars[2])
+                    self.use_tuning[key] = trial.suggest_float(
+                        key, low=pars[0], high=pars[1], step=pars[2]
+                    )
                 else:
                     raise TypeError("All parameters have to be of the same type")
             elif isinstance(pars, list):
                 self.use_tuning[key] = trial.suggest_categorical(key, pars)
             else:
                 self.use_tuning[key] = pars
-        
-        self.use_tuning["block_groups"] = [self.use_tuning["block_groups"]]*4
+
+        self.use_tuning["block_groups"] = [self.use_tuning["block_groups"]] * 4
 
         if self.use_tuning["optimizer"] == "Adam":
             hparams_keys = ["lr", "weight_decay"]
@@ -66,8 +86,13 @@ class ParameterOptimization:
         for key in ["block_groups", "block_name", "activation_name"]:
             val = self.use_tuning[key]
             model_hparams[key] = val
-        model_hparams["num_classes"] = self.model_config["net_hyperparams"]["num_classes"]
-        model_hparams["hidden_channels"] = self.model_config["net_hyperparams"]["hidden_channels"]
+
+        model_hparams["num_classes"] = self.model_config["net_hyperparams"][
+            "num_classes"
+        ]
+        model_hparams["hidden_channels"] = self.model_config["net_hyperparams"][
+            "hidden_channels"
+        ]
 
         self.model, self.result, _ = train_model(
             model_name=self.model_config["model_name"],
@@ -79,15 +104,28 @@ class ParameterOptimization:
             model_hparams=model_hparams,
             optimizer_name=self.use_tuning["optimizer"],
             optimizer_hparams=optimizer_hparams,
-            epochs=self.model_config["epochs"]
+            epochs=self.model_config["epochs"],
         )
 
         self.score = self.result["val"]
 
-    def objective(self, trial : Trial) -> float:
+    def objective(self, trial: Trial) -> float:
         self.make_network(trial)
         return self.score
-    
-    def optimize(self, study_name, direction, n_trials, n_jobs) -> None:
-        self.study = create_study(study_name=study_name, direction=direction, storage=self.optuna_path, load_if_exists=True)
-        self.study.optimize(lambda trial: self.objective(trial), n_trials=n_trials, n_jobs=n_jobs, show_progress_bar=True, gc_after_trial=True)
+
+    def optimize(
+        self, study_name: str, direction: str, n_trials: int, n_jobs: int
+    ) -> None:
+        self.study = create_study(
+            study_name=study_name,
+            direction=direction,
+            storage=self.optuna_path,
+            load_if_exists=True,
+        )
+        self.study.optimize(
+            lambda trial: self.objective(trial),
+            n_trials=n_trials,
+            n_jobs=n_jobs,
+            show_progress_bar=True,
+            gc_after_trial=True,
+        )
